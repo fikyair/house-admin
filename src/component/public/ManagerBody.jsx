40779da0ca5a65c82866dbfba701340fa61ee271
fikyair <shiming_xsm@163.com>
2018-03-26 23:01:56 +0800

    [UI]userTable

---
+++ src/component/public/ManagerBody.jsx
@@ -0,0 +1,101 @@
+import React from 'react';
+import {Card, Table, Pagination} from 'antd';
+import './style/tablebody.less'
+
+export default class ManagerBody extends React.Component {
+    columns = [{
+        key: 'id',
+        dataIndex: 'id',
+        title: "Openid 号"
+    }, {
+        key: 'mecFlg',
+        dataIndex: 'mecFlg',
+        title: "商户标识"
+    }, {
+        key: 'ruleNames',
+        dataIndex: 'ruleNames',
+        title: "规则名称"
+    }, {
+        key: 'id2',
+        dataIndex: 'id2',
+        title: "执行类型"
+    }, {
+        key: 'id3',
+        dataIndex: 'id3',
+        title: "规则描述"
+    }, {
+        key: 'id4',
+        dataIndex: 'id4',
+        title: "控制点"
+    }, {
+        key: 'id5',
+        dataIndex: 'id5',
+        title: "风险类型"
+    }, {
+        key: 'id6',
+        dataIndex: 'id6',
+        title: "处罚方式"
+    }, {
+        key: 'id7',
+        dataIndex: 'id7',
+        title: "创建人"
+    }, {
+        key: 'id8',
+        dataIndex: 'id8',
+        title: "创建时间"
+    }, {
+        key: 'id9',
+        dataIndex: 'id9',
+        title: "备注"
+    }, {
+        key: 'id0',
+        dataIndex: 'id0',
+        title: "操作"
+    }]
+
+    changePage = (page) => {
+        debugger;
+        const {pageSize, changePage, serachData} = this.props;
+        this.props.changePage(page);//子组件改变page时候通父组件
+        const params = {
+            pageNum: page,
+            pageSize,
+        }
+        serachData(params)
+    }
+
+
+    render() {
+        const {dataSource = [], columns, title, footer, pageNum, pageSize, total, changeSize, serachData} = this.props;
+        return (
+            <div style={{margin: '20px 10px'}}>
+                <Card noHovering={true} className="limitable" bodyStyle={{padding: '0px'}}>
+                    <Table
+                        style = {{margin: '0 20px', }}
+                        className="btl"
+                        columns={columns}
+                        dataSource={dataSource}
+                        title={() => title}
+                        footer={() => footer}
+                        pagination={false}
+                    />
+                    <div style={{margin: 29, textAlign: 'right'}}>
+                        <Pagination showSizeChanger
+                                    onShowSizeChange={(current, size) => {
+                                        changeSize(size);
+                                        const params = {
+                                            pageNum:parseInt(current) == 0? 1:current,
+                                            pageSize: size,
+                                        }
+                                        serachData(params)
+                                    }}
+                                    current={parseInt(pageNum)}
+                                    pageSize={parseInt(pageSize)}
+                                    total={parseInt(total)}
+                                    onChange={this.changePage}/>
+                    </div>
+                </Card>
+            </div>
+        )
+    }
+}
--- src/containers/user/userIndex.jsx
+++ src/containers/user/userIndex.jsx
@@ -1,101 +1,85 @@
-import React, { Component, PropTypes } from 'react'; // 引入了React和PropTypes
-import { connect } from 'react-redux';
-import { is, fromJS } from 'immutable';
-import Config from '../../config/index';
-import { Bcrumb } from '../../component/bcrumb/bcrumb';
-
-import styles from './style/user.less';
+import React, {Component, PropTypes} from 'react'; // 引入了React和PropTypes
+import {is, fromJS} from 'immutable';
+import {Bcrumb} from '../../component/bcrumb/bcrumb';
+import ManagerBody from "../../component/public/ManagerBody";
 
 /* 以类的方式创建一个组件 */
 class Main extends Component {
     constructor(props) {
-    	super(props);
+        super(props);
         this.state = {
             loading: false,
-            userInfo: []
+            userInfo: [],
+            pageNum: 1,
+            pageSize: 10,
+            total: 1,
         };
     }
-    shouldComponentUpdate(nextProps, nextState) {
-        return !is(fromJS(this.props), fromJS(nextProps)) || !is(fromJS(this.state),fromJS(nextState))
-    }
-    getUserInfo = (params={}) => {  // 加载用户列表信息
-    	this.setState({ loading: true });
-		this.props.getData('/user/userList', params, (res) => {  
-			this.saveLoading(false);
-            if(res.code == Config.errorCode.success) {
-            	const pagination = { ...this.state.pagination };
-            	pagination.total = res.recordsTotal; // 总页数
-            	pagination.current = params.page; // 当前页数
-            	this.setState({ 
-            		userInfo: res.data,
-			        pagination
-            	});
-         	} else {
-         		message.error(res.msg);
-         	}
-        }, 'userUsers', 'GET');
-	}
-	render() {
-     	const columns = [{
-			title: '用户名',
-			dataIndex: 'userName',
-			key: 'userName'
-		}, {
-			title: '姓名',
-			dataIndex: 'name',
-			key: 'name'
-		}, {
-			title: '邮箱',
-		  	dataIndex: 'email',
-		  	key: 'email'
-		}, {
-		  	title: '角色',
-		  	dataIndex: 'roleName',
-		  	key: 'roleName'
-		}, {
-		  	title: '创建时间',
-		  	dataIndex: 'createDate',
-		  	key: 'createDate',
-		  	render: (text, record) => (
-    			<span>
-     				{Config.formatDateTime(text)}
-    			</span>
-  			)
-		}, {
-		  	title: '操作',
-		  	key: 'action',
-		  	render: (text, record) => (
-		    	(record.roleLevel&&record.roleLevel>curRoleLevel) ? 
-			  		<span>
+
+    columns = [{
+        title: '用户名',
+        dataIndex: 'userName',
+        key: 'userName'
+    }, {
+        title: '姓名',
+        dataIndex: 'name',
+        key: 'name'
+    }, {
+        title: '昵称',
+        dataIndex: 'nickName',
+        key: 'nickName'
+    }, {
+        title: '电话',
+        dataIndex: 'phone',
+        key: 'phone'
+    }, {
+        title: '操作',
+        key: 'action',
+        render: (text, record) => (
+            (record.roleLevel && record.roleLevel > curRoleLevel) ?
+                <span>
 			    		<a href="javascript:;" onClick={this.editUser.bind(this, record)}>编辑</a>
-			    		<span className="ant-divider" />
-			    		<a href="javascript:;" className="ant-dropdown-link" onClick={this.changeState.bind(this, record.clientId, record.name, record.available)}>{record.available==1?'停用':'启用'}</a>
-			    		<span className="ant-divider" />
+			    		<span className="ant-divider"/>
+			    		<a href="javascript:;" className="ant-dropdown-link"
+                           onClick={this.changeState.bind(this, record.clientId, record.name, record.available)}>{record.available == 1 ? '停用' : '启用'}</a>
+			    		<span className="ant-divider"/>
 			    		<a href="javascript:;" onClick={this.delUser.bind(this, record.clientId, record.name)}>删除</a>
 			    	</span>
-				    :	
-			     	<span>
+                :
+                <span>
 			    		<span className="cole5e5e5">编辑</span>
 			    		<span className="ant-divider"/>
-			    		<span className="cole5e5e5">{record.available==1?'停用':'启用'}</span>
-			    		<span className="ant-divider" />
+			    		<span className="cole5e5e5">{record.available == 1 ? '停用' : '启用'}</span>
+			    		<span className="ant-divider"/>
 			    		<span className="cole5e5e5">删除</span>
 			    	</span>
-		  	)
-		}];
+        )
+    }];
 
-        let userInfo = this.state.userInfo;  // 用户信息数据 
-		return (	
-		   <div className="user-container">
-                <Bcrumb title="账号信息管理" icon="user" />
-			    账号信息管理
-		   </div>
-		);
-	}
+    shouldComponentUpdate(nextProps, nextState) {
+        return !is(fromJS(this.props), fromJS(nextProps)) || !is(fromJS(this.state), fromJS(nextState))
+    }
+
+    render() {
+
+        let userInfo = this.state.userInfo;  // 用户信息数据
+        return (
+            <div className="user-container">
+                <Bcrumb title="账号信息管理" icon="user"/>
+                <ManagerBody
+                    title={<span style = {{ fontSize: 13 ,fontWeight: 400 }}> 用户信息表 </span>}
+                    pageNum={ this.state.pageNum }
+                    pageSize={ this.state.pageSize }
+                    total={ this.state.total }
+                    columns={ this.columns }
+                    //dataSource = { }
+                />
+            </div>
+        );
+    }
 }
 
-Main.contextTypes = {
-};
+Main.contextTypes = {};
 
 export default Main;
 
