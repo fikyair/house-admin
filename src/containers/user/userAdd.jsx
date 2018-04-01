b33577e27c0c705d53894ff25bddc05c2cf7820e
fikyair <shiming_xsm@163.com>
2018-04-01 12:31:06 +0800

    [ui]addUser page

--- src/containers/user/userAdd.jsx
+++ src/containers/user/userAdd.jsx
@@ -1,13 +1,86 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {Bcrumb} from "../../component/bcrumb/bcrumb";
+import {Form, Button, Input, Row, Col, Card} from 'antd';
 
+const FormItem = Form.Item;
+
+@Form.create()
 class userAdd extends Component {
 
-    render () {
-        return(
+    render() {
+        const {getFieldDecorator} = this.props.form;
+        const formItemLayout = {
+            labelCol: {
+                xs: {span: 12},
+                sm: {span: 6},
+            },
+            wrapperCol: {
+                xs: {span: 12},
+                sm: {span: 6},
+            },
+        };
+        return (
             <div className="user-container">
                 <Bcrumb title="添加账号" icon="usergroup-add"/>
-                这里是增加哦
+                <Form>
+                    <Card title={'添加账号信息'}>
+                        <Row>
+                            <Col span={12}>
+                                <FormItem
+                                    label="用户名"
+                                    {...formItemLayout}
+                                >
+                                    {getFieldDecorator('uName', {
+                                            initialValue: ''
+                                        }
+                                    )(
+                                        <Input style={{width: 140}}/>
+                                    )}
+                                </FormItem>
+                            </Col>
+                            <Col span={12}>
+                                <FormItem
+                                    label="密码"
+                                    {...formItemLayout}
+                                >
+                                    {getFieldDecorator('uPwd', {
+                                            initialValue: ''
+                                        }
+                                    )(
+                                        <Input style={{width: 140}}/>
+                                    )}
+                                </FormItem>
+                            </Col>
+                            <Col span={12}>
+                                <FormItem
+                                    label="昵称"
+                                    {...formItemLayout}
+                                >
+                                    {getFieldDecorator('uNickname', {
+                                            initialValue: ''
+                                        }
+                                    )(
+                                        <Input style={{width: 140}}/>
+                                    )}
+                                </FormItem>
+                            </Col>
+                            <Col span={12}>
+                                <FormItem
+                                    label="电话"
+                                    {...formItemLayout}
+                                >
+                                    {getFieldDecorator('uPhone', {
+                                            initialValue: ''
+                                        }
+                                    )(
+                                        <Input style={{width: 140}}/>
+                                    )}
+                                </FormItem>
+                            </Col>
+                        </Row>
+                    </Card>
+                </Form>
+                <Button type ="primary" htmlType="submit" style = {{ marginLeft: '69%', marginTop: 20 }}>提交</Button>
             </div>
         )
     }
